e014e857a6400df2907340f0ca0e99303e05b1ee
Ben Dwyer <ben@scruffian.com>
2017-08-11 22:54:29 +0100

    Add layout for woo

--- client/layout/logged-out.jsx
+++ client/layout/logged-out.jsx
@@ -10,33 +10,88 @@ import { connect } from 'react-redux';
  * Internal dependencies
  */
 import MasterbarLoggedOut from 'layout/masterbar/logged-out';
+import { getOAuth2ClientData, showOAuth2Layout } from 'state/login/selectors';
 import { getSection } from 'state/ui/selectors';
 import OauthClientLayout from 'layout/oauth-client';
-import { showOAuth2Layout } from 'state/login/selectors';
+
+const clients = {
+	930: {
+		name: 'vaultpress',
+		img_url: 'https://vaultpress.com/images/vaultpress-wpcc-nav-2x.png',
+		img_height: 50,
+		img_width: 70,
+	},
+	973: {
+		name: 'akismet',
+		img_url: 'https://akismet.com/img/akismet-wpcc-logo-2x.png',
+		img_height: 50,
+		img_width: 70,
+	},
+	978: {
+		name: 'polldaddy',
+		img_url: 'https://polldaddy.com/images/polldaddy-wpcc-logo-2x.png',
+		img_height: 50,
+		img_width: 70,
+	},
+	1854: {
+		name: 'gravatar',
+		img_url: 'https://gravatar.com/images/grav-logo-2x.png',
+		img_height: 50,
+		img_width: 70,
+	},
+	50019: {
+		name: 'woo',
+		img_url: 'https://woocommerce.com/wp-content/themes/woomattic/images/logo-woocommerce@2x.png',
+		img_height: 41,
+		img_width: 200,
+	},
+	50915: {
+		name: 'woo',
+		img_url: 'https://woocommerce.com/wp-content/themes/woomattic/images/logo-woocommerce@2x.png',
+		img_height: 41,
+		img_width: 200,
+	},
+	50916: {
+		name: 'woo',
+		img_url: 'https://woocommerce.com/wp-content/themes/woomattic/images/logo-woocommerce@2x.png',
+		img_height: 41,
+		img_width: 200,
+	},
+};
 
 const LayoutLoggedOut = ( {
+	oauth2ClientData,
 	primary,
 	section,
 	redirectUri,
 	useOAuth2Layout,
 } ) => {
-	const classes = classNames( 'layout', {
+	let client = null;
+	if ( oauth2ClientData && oauth2ClientData.id in clients ) {
+		client = clients[ oauth2ClientData.id ];
+	}
+
+	const classNameObject = {
 		[ 'is-group-' + section.group ]: !! section,
 		[ 'is-section-' + section.name ]: !! section,
 		'focus-content': true,
 		'has-no-sidebar': true, // Logged-out never has a sidebar
 		'wp-singletree-layout': !! primary,
-	} );
+		dops: !! client,
+	};
 
-	if ( useOAuth2Layout ) {
-		return (
-			<OauthClientLayout primary={ primary } />
-		);
+	if ( client ) {
+		classNameObject[ client.name ] = !! client;
+	}
+
+	let masterbar = <MasterbarLoggedOut title={ section.title } sectionName={ section.name } redirectUri={ redirectUri } />;
+	if ( useOAuth2Layout && client ) {
+		masterbar = <OauthClientLayout client={ client } />;
 	}
 
 	return (
-		<div className={ classes }>
-			<MasterbarLoggedOut title={ section.title } sectionName={ section.name } redirectUri={ redirectUri } />
+		<div className={ classNames( 'layout', classNameObject ) }>
+			{ masterbar }
 			<div id="content" className="layout__content">
 				<div id="primary" className="layout__primary">
 					{ primary }
@@ -63,6 +118,7 @@ LayoutLoggedOut.propTypes = {
 export default connect(
 	state => ( {
 		section: getSection( state ),
+		oauth2ClientData: getOAuth2ClientData( state ),
 		useOAuth2Layout: showOAuth2Layout( state ),
 	} )
 )( LayoutLoggedOut );
